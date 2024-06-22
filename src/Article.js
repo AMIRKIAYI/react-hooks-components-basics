4c833767e9749d2a5e743c4b33a9e98f2b93c421
AMIRKIAYI <amirkiayi516@gmail.com>
2024-06-22 14:59:42 +0300

    all files up to date

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Article() {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>Dear Amir: Bjarne Stroustrup has the perfect lecture oration.Please go through this lecture to gain more from it.</div>
   );
 }
 
--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return <div>Naturally, I agree with this article and i am ready now to read hardly either in harsh or smooth situation. Let it be good or bad, All in all i put God first and move on.</div>;
 }
 
 export default Comment;
